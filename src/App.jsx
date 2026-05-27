91f76228a450568ee3725a06061b0c4921258234
GeraldineRomer <geraldine.rome0104@gmail.com>
2026-05-27 16:12:12 -0500

    feat: creación completa del footer con acceso de links

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Projects from "./components/Projects"
 import Timeline from "./components/Timeline"
 import LearningLog from "./components/LearningLog"
 import Contact from "./components/Contact"
+import Footer from "./components/Footer"
 
 export default function App() {
   return (
@@ -20,6 +21,7 @@ export default function App() {
       <Timeline />
       <LearningLog />
       <Contact />
+      <Footer />
     </main>
   )
 }
